e1235ab930879a6f341a39e078cbc16111e40bdd
Valeri Karpov <val@karpov.io>
2018-07-12 17:33:10 -0400

    chore: add some comments

--- cypress/integration/simple.js
+++ cypress/integration/simple.js
@@ -2,6 +2,8 @@ describe('Autocomplete', function() {
   it('renders results', function() {
     cy.visit('http://localhost:5000', {
       onBeforeLoad: win => {
+        // Stub out `window.fetch()` so `index.js` doesn't actually hit
+        // the npm API
         cy.stub(win, 'fetch', () => {
           return Promise.resolve({
             json: () => ({
@@ -15,8 +17,12 @@ describe('Autocomplete', function() {
       }
     });
 
+    // `{enter}` is how you simulate hitting the 'enter' key in Cypress
+    // See: https://docs.cypress.io/api/commands/type.html#Arguments
     cy.get('#search').type('mongoose{enter}');
 
+    // Don't use `expect()` directly here, chaining calls onto `cy`
+    // is potentially async
     cy.get('#results').
       should('contain', 'mongoose').
       and('contain', 'mongoose-autopopulate');
